5ce0732b6d55e506ca5b781cbb2ac4eee57a0253
Anna <annakupyniak@gmail.com>
2026-03-13 00:14:28 +0200

    Add Serilog and Seq configuration

--- Enrich/Enrich.Web/Program.cs
+++ Enrich/Enrich.Web/Program.cs
@@ -1,45 +1,65 @@
+using Serilog;
 using Microsoft.EntityFrameworkCore;
 using Enrich.Infrastructure.Models;
-namespace Enrich.Web
 
+namespace Enrich.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-           
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql(connectionString));
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-            
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            try
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                Log.Information("Запуск веб-хоста...");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext());
+
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql(connectionString));
+
+                builder.Services.AddControllersWithViews();
+
+                var app = builder.Build();
+
+                Log.Information("Додаток Enrich запущено");
+
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.UseAuthorization();
+
+                app.MapStaticAssets();
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}")
+                    .WithStaticAssets();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Додаток завершився критичною помилкою під час запуску");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
