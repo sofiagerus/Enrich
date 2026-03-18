2987be63063d4f2f99b0c8d2f4afbb4c5a3a8541
Your Name <bohdan.lol66@gmail.com>
2026-03-18 13:56:04 +0200

    Refactor validations

---
+++ Enrich/Enrich.BLL/Validations/UserValidationRules.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+public static class UserValidationRules
+{
+    public static IRuleBuilderOptions<T, string> ApplyPasswordRules<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+                .NotEmpty().WithMessage("Пароль є обов'язковим")
+                .MinimumLength(8).WithMessage("Пароль має містити мінімум 8 символів")
+                .Matches("[A-Z]").WithMessage("Потрібна хоча б одна велика літера");
+    }
+
+    public static IRuleBuilderOptions<T, string> ApplyUsernameRules<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+                .NotEmpty().WithMessage("Ім'я користувача є обов'язковим")
+                .MinimumLength(3).WithMessage("Ім'я має містити мінімум 3 символи")
+                .MaximumLength(16).WithMessage("Ім'я не може бути довшим за 16 символів")
+                .Matches("^[a-zA-Z._]*$").WithMessage("Дозволені лише літери латинського алфавіту, крапка та нижнє підкреслення");
+    }
+}
--- Enrich/Enrich.Web/Validators/SignupViewModelValidator.cs
+++ Enrich/Enrich.Web/Validators/SignupViewModelValidator.cs
@@ -11,16 +11,9 @@ namespace Enrich.Web.Validators
                 .NotEmpty().WithMessage("Електронна пошта є обов'язковою")
                 .EmailAddress().WithMessage("Неправильний формат пошти");
 
-            RuleFor(x => x.Username)
-                .NotEmpty().WithMessage("Ім'я користувача є обов'язковим")
-                .MinimumLength(3).WithMessage("Ім'я має містити мінімум 3 символи")
-                .MaximumLength(20).WithMessage("Ім'я не може бути довшим за 20 символів");
+            RuleFor(x => x.Username).ApplyUsernameRules();
 
-            RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Пароль є обов'язковим")
-                .MinimumLength(8).WithMessage("Пароль має містити мінімум 8 символів")
-                .Matches("[A-Z]").WithMessage("Пароль повинен містити хоча б одну велику літеру")
-                .Matches("[0-9]").WithMessage("Пароль повинен містити хоча б одну цифру");
+            RuleFor(x => x.Password).ApplyPasswordRules();
         }
     }
 }
