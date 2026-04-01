8c472fcc106ee6667affb570a1eeefdab3c9d7af
Anastasia Tumak <anastasiatumakk@gmail.com>
2026-04-01 21:01:53 +0300

    update

--- Enrich/Enrich.BLL/Constants/BundleConstants.cs
+++ Enrich/Enrich.BLL/Constants/BundleConstants.cs
@@ -2,11 +2,11 @@ namespace Enrich.BLL.Constants
 {
     public static class BundleConstants
     {
-        public const string TitleRequired = "Назва бандлу є обов'язковою";
-        public const string TitleMaxLengthMessage = "Назва бандлу не може перевищувати 200 символів";
+        public const string TitleRequired = "Collection title is required";
+        public const string TitleMaxLengthMessage = "Collection title cannot exceed 200 characters";
         public const int TitleMaxLength = 200;
 
-        public const string DescriptionMaxLengthMessage = "Опис бандлу не може перевищувати 1000 символів";
+        public const string DescriptionMaxLengthMessage = "Collection description cannot exceed 1000 characters";
         public const int DescriptionMaxLength = 1000;
 
         public const string ImageUrlMaxLengthMessage = "Image cannot exceed 2 MB";
